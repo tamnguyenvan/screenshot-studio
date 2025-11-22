706f6030fbf4de5bdfd62daa4b7b21fc8694cd88
Nguyen Van Tam <tamnvhustcc@gmail.com>
2025-11-22 16:30:06 +0700

    chore: SEO

--- App.tsx
+++ App.tsx
@@ -148,7 +148,7 @@ const App: React.FC = () => {
             <div className="absolute top-0 left-0 z-50 p-6 pointer-events-none">
                 <div className="flex items-center gap-3 pointer-events-auto">
                     <div className="w-8 h-8 bg-white rounded-lg flex items-center justify-center text-black p-1">
-                        <svg viewBox="0 0 100 100" aria-hidden="true" focusable="false" className="w-full h-full">
+                        <svg viewBox="0 0 100 100" aria-hidden="true" focusable="false" className="w-full h-full text-blue-500">
                             <path d="M0.5 0C0.367392 0 0.240215 0.0526784 0.146447 0.146447C0.0526784 0.240215 0 0.367392 0 0.5L0 1H0.5C0.632608 1 0.759785 0.947322 0.853553 0.853553C0.947322 0.759785 1 0.632608 1 0.5V0H0.5ZM0.5 0.75C0.433696 0.75 0.370107 0.723661 0.323223 0.676777C0.276339 0.629893 0.25 0.566304 0.25 0.5C0.25 0.433696 0.276339 0.370107 0.323223 0.323223C0.370107 0.276339 0.433696 0.25 0.5 0.25C0.566304 0.25 0.629893 0.276339 0.676777 0.323223C0.723661 0.370107 0.75 0.433696 0.75 0.5C0.75 0.566304 0.723661 0.629893 0.676777 0.676777C0.629893 0.723661 0.566304 0.75 0.5 0.75Z" fill="currentColor" transform="scale(100)"></path>
                         </svg>
                     </div>
